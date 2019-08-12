f6a68d881df1fe862e7aa4c9329fba7719d0eb6a
Sebastian Carrera <sebascarreram@hotmail.com>
2019-08-12 14:04:10 -0500

    push day06

---
+++ day06/ex01/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarrera <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 21:47:54 by scarrera          #+#    #+#             */
+/*   Updated: 2019/07/17 23:38:57 by scarrera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_program_name(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char *argv[])
+{
+	ft_print_program_name(argv[0]);
+	return (0);
+}
