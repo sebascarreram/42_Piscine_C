dc3bb7adf08b585f1174901f829ba9bd07121481
Sebastian Carrera <sebascarreram@hotmail.com>
2019-09-12 22:00:44 -0500

    solve and organized lines

--- day04/ex01/ft_recursive_factorial.c
+++ day04/ex01/ft_recursive_factorial.c
@@ -13,10 +13,12 @@
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb > 1 && nb < 13)
-		return (nb * ft_recursive_factorial(nb - 1));
+	if (nb < 0)
+		return (0);
 	else if (nb == 0 || nb == 1)
 		return (1);
+	else if (nb > 1 && nb < 13)
+		return (nb * ft_recursive_factorial(nb - 1));
 	else
 		return (0);
 }
@@ -25,7 +27,7 @@ int main()
 {
 	int test;
 	
-	test = 12;
+	test = 13;
 	printf("%i\n", ft_recursive_factorial(test));
 	return (0);
 }
