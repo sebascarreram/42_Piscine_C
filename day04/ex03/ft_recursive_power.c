b3cd8cbb57bbe690394312bbe485b78b6ee6c713
Sebastian Carrera <sebascarreram@hotmail.com>
2019-09-12 22:55:32 -0500

    solve and missing a if for power = 0 return 1

--- day04/ex03/ft_recursive_power.c
+++ day04/ex03/ft_recursive_power.c
@@ -10,14 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_recursive_power(int nb, int power)
 {
 	if (power == 1)
-	{
 		return (nb);
-	}
+	else if (power == 0)
+		return (1);
 	else
-	{
 		return (nb * ft_recursive_power(nb, power - 1));
-	}
+}
+// NOT MAIN NOT #INCLUDE FOR SUBMIT
+int main()
+{
+	int num1;
+	int num2;
+	
+	num1 = 4;
+	num2 = 0;
+	printf("%i\n", ft_recursive_power(num1,num2));
+	return (0);
 }
