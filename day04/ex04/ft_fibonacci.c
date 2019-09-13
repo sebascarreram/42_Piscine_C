8c3fe3461cf366284d8f47ae96ce765ffc546c76
Sebastian Carrera <sebascarreram@hotmail.com>
2019-09-13 15:44:28 -0500

    solved and organized lines

--- day04/ex04/ft_fibonacci.c
+++ day04/ex04/ft_fibonacci.c
@@ -9,19 +9,23 @@
 /*   Updated: 2019/07/13 23:29:25 by scarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 int	ft_fibonacci(int index)
 {
-	if (index == 0)
-	{
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
 		return (0);
-	}
-	if (index == 1 || index == 2)
-	{
+	else if (index == 1)
 		return (1);
-	}
-	else
-	{
-		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
-	}
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+// not include not main for submit
+int main()
+{
+	int number = 6;
+
+	printf("%i\n", ft_fibonacci(number));
+	return (0);
 }
