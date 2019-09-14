dac70c15dc089f80f0a8f4194ed44d3845afd60e
Sebastian Carrera <sebascarreram@hotmail.com>
2019-09-14 11:57:56 -0500

    solved

--- day05/ex03/ft_strcpy.c
+++ day05/ex03/ft_strcpy.c
@@ -9,17 +9,28 @@
 /*   Updated: 2019/07/19 13:39:40 by scarrera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
 
 char *ft_strcpy(char *dest, char*src)
 {
 	int index;
 
 	index = 0;
-	while (str[index] ==  '\0')
+	while (src[index] !=  '\0')
 	{
 		dest[index] = src[index];
 		++index;
 	}
 	dest[index] = '\0';
 	return (dest);
+}
+// NOT #INCLUDE NOT MAIN FOR SUBMIT
+int main()
+{
+	char name[] = "sebas";
+	char lastname[20];
+	
+	ft_strcpy(lastname, name);
+	printf("%s\n", lastname);
+	return (0);
 }
