a1d66c7c8bf088106cca6979fa45c3ae3ec2bb3e
Sebastian Carrera <sebascarreram@hotmail.com>
2019-09-24 13:11:14 -0500

    added somethings in main for fun

--- day04/ex06/ft_is_prime.c
+++ day04/ex06/ft_is_prime.c
@@ -25,13 +25,22 @@ int	ft_is_prime(int nb)
 
 int	main()
 {
-	int number1;
-	
+	int number1[] = {104395303, 39, 29, 65, 8,7 ,'\0'};
+	int index;
+
+	index = 0;
 	//number1 = 104395303;
 	//number1 = 39;
 	//number1 = 29;
-	number1 = 65;
+	//number1 = 65;
 	//number1 = 8;
-	printf("%i\n", ft_is_prime(number1));
+	while (number1[index] != '\0')
+	{
+		if (ft_is_prime(number1[index]) == 0)
+			printf("%i is NOT prime = %i\n", number1[index], ft_is_prime(number1[index]));
+		else
+			printf("%i is prime = %i\n", number1[index], ft_is_prime(number1[index]));
+		index++;
+	}
 	return (0);
 }
