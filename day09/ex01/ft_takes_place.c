e089e9c03a5eb5246a2237a8404bd78c0fdacef7
Sebastian Carrera <sebascarreram@hotmail.com>
2019-10-10 10:52:05 -0500

    update

--- day09/ex01/ft_takes_place.c
+++ day09/ex01/ft_takes_place.c
@@ -18,31 +18,21 @@ void	ft_takes_place(int hour)
 
 	i = hour;
 	if (hour > 12)
-	{
 		i = hour % 12;
-	}
 	else if (hour == 0)
-	{
 		i = 12;
-	}
-	if (hour < 11)
-	{
-		printf("THE FOLLOWING TAKES PLACE BETWEEN %d.00 A.M. AND %d.00 A.M.\n", i, (i + 1) % 12);
+	if (hour < 11){
+		printf("THE FOLLOWING TAKES PLACE BETWEEN %d.00 
+				A.M. AND %d.00 A.M.\n", i, (i + 1) % 12);
 	}
 	else if (hour == 11)
-	{
 		printf("THE FOLLOWING TAKES PLACE BETWEEN 11.00 A.M. AND 12.00 P.M.\n");
-	}
 	else if (hour == 23)
-	{
 		printf("THE FOLLOWING TAKES PLACE BETWEEN 11.00 P.M. AND 12.00 A.M.\n");
-	}
 	else if (hour == 24)
-	{
 		printf("THE FOLLOWING TAKES PLACE BETWEEN 12.00 A.M. AND 01.00 A.M.");
-	}
-	else
-	{
-		printf("THE FOLLOWING TAKES PLACE BETWEEN %d.00 P.M. AND %d.00 P.M.\n", i, (i + 1) % 12);
+	else{
+		printf("THE FOLLOWING TAKES PLACE BETWEEN %d.00 
+				P.M. AND %d.00 P.M.\n", i, (i + 1) % 12);
 	}
 }
